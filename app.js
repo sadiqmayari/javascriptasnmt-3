c0c72a09e4308a72031da3a1a4d7dcd547533153
sadiqmayari <sadiqmayari@gmail.com>
2023-09-25 14:06:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// declare variable age then assign and alert
+var age;
+age="41";
+
+alert("I am " + age + " years old")
+
+// declare and initailize visits in variable
+
+var N =14;
+
+var messageFirstHalf= "you have visited this site ";
+var messageSecondtHalf=" times"
+
+alert(messageFirstHalf+N+messageSecondtHalf);
+
+//birth year in browser
+
+var birthYear = 1982;
+var textBefore = "My Birth Year is";
+var textAfter = "My type of declared variable is number";
+
+
+document.write(textBefore+birthYear+"<br>"+textAfter);
+
+//script for store visitors order details
+
+var userName = "<b>Jhone Doe</b> ";
+var productQuantity = 5;
+var productTitle = "<b> T-Shirts</b>";
+
+document.write("<br><br><br>"+userName+"orderd "+"<b>"+productQuantity+"</b>"+productTitle+" on XYZ Clothing Store");
